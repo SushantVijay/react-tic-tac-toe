806885a6d927f1c22e0fee5116198dc810ea2600
Sushant <sushantv.cs2022@citchennai.net>
2024-05-22 19:55:38 +0530

    first commit

---
+++ src/Info.js
@@ -0,0 +1,15 @@
+// Filename - Info.js
+
+// Importing the css for the info
+import "./css/info.css";
+
+const Info = () => {
+	return (
+		<div className="info">
+			<div className="player">Defy: X</div>
+			<div className="player">Madara: O</div>
+		</div>
+	);
+};
+
+export default Info;
